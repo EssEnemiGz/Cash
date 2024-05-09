08e7c64d4b4f3f618ed82499dc8d131b19d44379
EssEnemiGz <biscenp@gmail.com>
2024-05-09 19:31:55 -0400

    "Created index, components updated, auto header system added and more CSS changes"

--- static/js/containers-loader.js
+++ static/js/containers-loader.js
@@ -1,11 +1,10 @@
-
 // HTML CONTENT
-fetch("/static/components/header.html").then(response => response.text()).then(data => {
-    document.querySelector(".header").innerHTML = data;
-});
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/static/components/header.html").then(response => response.text()).then(data => {
+        document.querySelector(".header").innerHTML = data;
+    });
 
-// DEPENDENCIES LOADER
-document.addEventListener("DOMContentLoaded", function(){
+    // DEPENDENCIES LOADER
     head = document.querySelector("head");
     fetch("/static/components/dependencies.html").then(response => response.text()).then(data => {
         head.innerHTML += "\n";
--- static/js/script.js
+++ static/js/script.js
@@ -1,57 +1,127 @@
 // Datos para la gráfica de barras
-const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+const labels = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
 let data = [];
 
 let myChart = null;
-let myChart2 = null;
-let myChart3 = null;
+let ganancias = null;
+let horas_trabajadas = null;
+
 
 function destruirGraficas() {
     if (myChart) {
         myChart.destroy();
     }
-    if (myChart2) {
-        myChart2.destroy();
+    if (ganancias){
+        ganancias.destroy();
     }
-    if (myChart3) {
-        myChart3.destroy();
+    if (horas_trabajadas){
+        horas_trabajadas.destroy();
     }
+    return 0;
 }
 
-function generarGráfica(){
+function generarGrafica(){
     destruirGraficas();
     // Crear la instancia de la gráfica
-    const ctx = document.getElementById('myChart').getContext('2d');
-    myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Ventas por mes',
-                data: data,
-                backgroundColor: 'black', // Color de las barras
-                borderColor: '#333333', // Borde de las barras
-                borderWidth: 1
-            }]
-        },
-        options: {
-            pointRadius: 0,
-            tension: 0.3, // Smoth lines
-            scales: {
-                x: {
-                    grid: {
-                        drawOnChartArea: false,
-                        drawTicks: false
+    let ctx = document.getElementById('myChart')
+    if (ctx !== null){
+        ctx = ctx.getContext('2d');
+        myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Ventas por mes',
+                    data: data,
+                    backgroundColor: 'black', // Color de las barras
+                    borderColor: '#333333', // Borde de las barras
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                pointRadius: 0,
+                tension: 0.3, // Smoth lines
+                scales: {
+                    x: {
+                        grid: {
+                            drawOnChartArea: false,
+                            drawTicks: false
+                        }
+                    },
+                    y: {
+                        beginAtZero: true, // Empezar desde cero en el eje Y
                     }
-                },
-                y: {
-                    beginAtZero: true, // Empezar desde cero en el eje Y
                 }
-            }
-    }});
+        }});
+    }
+
+    const ctx2 = document.getElementById('ganancias');
+    if (ctx2 !== null){
+        ctx2 = ctx2.getContext();
+        myChart = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Ventas por mes',
+                    data: data,
+                    backgroundColor: 'black', // Color de las barras
+                    borderColor: '#333333', // Borde de las barras
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                pointRadius: 0,
+                tension: 0.3, // Smoth lines
+                scales: {
+                    x: {
+                        grid: {
+                            drawOnChartArea: false,
+                            drawTicks: false
+                        }
+                    },
+                    y: {
+                        beginAtZero: true, // Empezar desde cero en el eje Y
+                    }
+                }
+        }});
+    }
+
+    const ctx3 = document.getElementById('horas-trabajadas')
+    if (ctx3 !== null){
+        ctx3 = ctx3.getContext();
+        myChart = new Chart(ctx3, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Ventas por mes',
+                    data: data,
+                    backgroundColor: 'black', // Color de las barras
+                    borderColor: '#333333', // Borde de las barras
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                pointRadius: 0,
+                tension: 0.3, // Smoth lines
+                scales: {
+                    x: {
+                        grid: {
+                            drawOnChartArea: false,
+                            drawTicks: false
+                        }
+                    },
+                    y: {
+                        beginAtZero: true, // Empezar desde cero en el eje Y
+                    }
+                }
+        }});
+    }
+    return 0;
 }
 
-generarGráfica()
+generarGrafica()
 
 function cargarJSON() {
     const file = document.getElementById('up__input').files[0];
@@ -63,7 +133,7 @@ function cargarJSON() {
         try {
           const objetoJSON = JSON.parse(event.target.result);
           data = objetoJSON;
-          generarGráfica();
+          generarGrafica();
 
         } catch (error) {
           console.error(error);
